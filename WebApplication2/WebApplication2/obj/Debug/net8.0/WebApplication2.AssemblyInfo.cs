116a50dfe70a8198ccc336730003123a44548de1
lucasrodisic <lucasrodisic7@gmail.com>
2024-03-29 23:08:25 +0200

    12

--- WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012cd07338843c3d8cf9b329e4cf2081684399e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397ab9308f8792b85cbfc18b9568455d4affbe4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
